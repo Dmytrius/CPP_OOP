13511010f4fa431fab53f3356ebbc3d9ed8dc676
BoikoDM <boikodm@megabank.ua>
2019-06-08 07:10:50 +0300

    some smalle changes

--- main.cpp
+++ main.cpp
@@ -1,44 +1,10 @@
 #include <iostream>
-#include "interfaces/Date.h"
-#include "interfaces/Person.h"
-#include "interfaces/Author.h"
-#include "interfaces/Article.h"
+
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
+int main( int   argc,char *argv[] )
+{
 
-    cout << "Enter number for action: " << endl;
-    cout << "Press 1 for enter date: " << endl;
-    cout << "Press 2 for enter Person: " << endl;
-    cout << "Press 3 for enter Author: " << endl;
-    cout << "Press 4 for enter Post: " << endl;
-    cout << "Press 5 for enter Journal: " << endl;
-    int button;
-    cin >> button;
-    switch (button) {
-        case 1: {
-            cout << "Please enter date: ";
-            int day, month, year;
-            cout << "Enter day: ";
-            cin >> day;
-            cout << "Enter month: ";
-            cin >> month;
-            cout << "Enter year: ";
-            cin >> year;
-            Date *date = new Date(day, month, year);
-        }
-        case 2: {
-
-        }
-        case 3: {
-
-        }
-        case 4: {
-
-        }
-        case 5: {
-
-        }
-    }
+    return 0;
 }
